f6f31c7eb8eae2008edd629e0ca3311dc0daf0e0
JJFAO <jjfalonso@gmail.com>
2021-08-17 05:04:02 -0300

    feat(Navbar): add logo and title

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,16 @@
 import { Button, Container, Navbar } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 export default function NavBar({ children }) {
   return (
     <Navbar expand="lg" bg="dark" variant="dark" sticky="top">
       <Container fluid>
         <Navbar.Brand as={Link} to="/">
-          Rick & Morty
+          <img style={{width: '6.5rem'}} src={logo} alt="logo" />
         </Navbar.Brand>
 
-        <Button as={NavLink} to="/favorites" className="py-1" variant="outline-primary">
+        <Button as={NavLink} to="/favorites" className="py-1 me-auto ms-4" variant="outline-primary">
           Favoritos
         </Button>
 
